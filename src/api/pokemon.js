d3a74675608b08c2ba1626a32c138b67dc54761e
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-09-14 20:21:14 -0400

    Switched to using the more global 151 pokemon API call

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,15 +1,4 @@
-async function fetchPokemon() {
-  // get 20 pokemon in an array
-  const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
-
-  const twentyNums = () => Array.from({ length: 20 }, (_, i) => i + 1);
-
-  const builtUrl = twentyNums.map((num) => {
-    baseUrl + `${num}/`;
-  });
-
-  console.log(builtUrl);
-
+export default async function fetchPokemon() {
   // getting the list of 151 pokemon
   try {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
@@ -17,10 +6,23 @@ async function fetchPokemon() {
     if (!response.ok) {
       throw new Error(`HTTP error! Status code: ${response.status}`);
     }
-    console.log(response.status);
+
+    // console.log(response.status);
 
     const pokemon = await response.json();
     console.log(pokemon);
+
+    // console.log(pokemon.results[0].name) gets the name of the pokemon
+    // console.log(pokemon.results[0].url) gets the url for the pokemon
+
+    const url = new URL(pokemon.results[0].url);
+    console.log(url);
+
+    // to get the id of the pokemon, use the pathname property returned from the split
+    const splitUrl = url.pathname.split("/");
+    console.log(splitUrl);
+
+    // get the first 20 url from the pokemon api response
   } catch (error) {
     console.error(`Fetch failed:`, error);
   }

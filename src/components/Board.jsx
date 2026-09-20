b74b3da5930cf24909aa2e756dc3a098a85f180e
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-09-19 20:14:03 -0400

    Rendered pokemon objects to page
    - Used the fisher yates algorithm to return 9 random pokemon objects
    - Rendered the objects to the screen (sprite and name)

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,16 +1,49 @@
 import fetchPokemon from "../api/pokemon";
+import Card from "./Card";
 import { useState, useEffect } from "react";
 
 export default function Board() {
   const [pool, setPool] = useState([]);
+  const [board, setBoard] = useState([]);
+
   useEffect(() => {
     fetchPokemon()
       .then((data) => {
         setPool(data);
+        const nineRandomPokemonObjs = getNinePokemonObj(data);
+
+        setBoard(nineRandomPokemonObjs);
       })
       .catch((error) => {
         console.error("An error occurred", error);
       });
   }, []);
-  console.log(pool);
+
+  function handleClick() {
+    const { name, value } = e.target;
+  }
+
+  return (
+    <div className="grid grid-cols-3">
+      {board.map((pokemon) => {
+        return (
+          <Card
+            key={pokemon.id}
+            pokemonId={pokemon.id}
+            name={pokemon.name}
+            onClick={() => {}}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function getNinePokemonObj(arr) {
+  const arrCopy = [...arr];
+  for (let i = 0; i < 9; i++) {
+    const randomIndex = Math.floor(Math.random() * (arrCopy.length - i) + i);
+    [arrCopy[i], arrCopy[randomIndex]] = [arrCopy[randomIndex], arrCopy[i]];
+  }
+  return arrCopy.slice(0, 9);
 }
